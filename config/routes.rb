282ec469f4a684317fb0abe547fa0016a99d3416
Jack Toal <toaljack@gmail.com>
2016-07-03 02:00:53 +0000

    Created pages controller. Explore.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+ 
+ 
+ # Define Roo URL
+ root 'pages#index'
+ 
+ # Define Routes for Pages
+  get '/home' => 'pages#home'
+  get '/profile' => 'pages#profile'
+  get '/explore' => 'pages#explore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
